989009f364aef4e795c594077cbe556d3380aeb5
repllca <maridann1022@icloud.com>
2025-01-01 22:05:09 +0900

    add login.html form

--- src/server.py
+++ src/server.py
@@ -1,11 +1,21 @@
 from sanic import Sanic
-from sanic.response import text
+from sanic.response import text,json
 
 app = Sanic("app")
 
 @app.route("/")
 async def main(request):
     return text("hello world")
+
+
+@app.route("/form", methods=["POST"])
+async def handle_form(request):
+    print("POST /form - リクエスト受信:", request.form)
+    password = request.form.get("email")
+    email = request.form.get("password")
+    return json({"password": password, "email": email})
+
+
 
 @app.get("/users")
 async def users(request):
